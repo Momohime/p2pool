189238b6fd2ca9ed8d725798aa6bbe5417df04fc
devlin85 <devlin.skahill@gmail.com>
2015-03-25 13:21:02 -0400

    Create digibyteSHA.py

---
+++ p2pool/networks/digibyteSHA.py
@@ -0,0 +1,18 @@
+from p2pool.bitcoin import networks
+
+PARENT = networks.nets['digibyteSHA']
+SHARE_PERIOD = 10 # seconds target spacing
+CHAIN_LENGTH = 24*60*60//10 # shares
+REAL_CHAIN_LENGTH = 24*60*60//10 # shares
+TARGET_LOOKBEHIND = 50 # shares diff regulation
+SPREAD = 10 # blocks
+IDENTIFIER = '48a4ebc31b798115'.decode('hex')
+PREFIX = '5685a276c2dd81db'.decode('hex')
+P2P_PORT = 5010
+MIN_TARGET = 0
+MAX_TARGET = 2**256//2**32 - 1
+PERSIST = False
+WORKER_PORT = 5011
+BOOTSTRAP_ADDRS = 'dgbsha.mastercryptopool.net dgbsha2.mastercryptopool.net dgbsha3.mastercryptopool.net'.split(' ')
+ANNOUNCE_CHANNEL = '#p2pool-alt'
+VERSION_CHECK = lambda v: True
