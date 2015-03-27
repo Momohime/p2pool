45e739cea26d5dd2391372148b275b01b8ef2d95
devlin85 <devlin.skahill@gmail.com>
2015-03-27 01:08:40 -0400

    DigitalCoin Testing

--- p2pool/bitcoin/networks/digitalcoinSha.py
+++ p2pool/bitcoin/networks/digitalcoinSha.py
@@ -17,12 +17,12 @@ RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
         ))
 SUBSIDY_FUNC = lambda height: 15*100000000 >> (height + 1)//4730400
 POW_FUNC = data.hash256
-BLOCK_PERIOD = 40 # s targetspacing
+BLOCK_PERIOD = 30 # s targetspacing
 SYMBOL = 'DGC'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'digitalcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/digitalcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.digitalcoin'), 'digitalcoin.conf')
 BLOCK_EXPLORER_URL_PREFIX = 'http://dgc.cryptocoinexplorer.com/block/'
 ADDRESS_EXPLORER_URL_PREFIX = 'http://dgc.cryptocoinexplorer.com/address/'
 TX_EXPLORER_URL_PREFIX = 'http://dgc.cryptocoinexplorer.com/transaction/'
-SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
-DUMB_SCRYPT_DIFF = 1
+SANE_TARGET_RANGE = (2**256//1000000000 - 1, 2**256//1000 - 1)
+DUMB_SCRYPT_DIFF = 2**16
 DUST_THRESHOLD = 0.001e8
