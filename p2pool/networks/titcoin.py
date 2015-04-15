62adb30ddb7ea5f0ec12afb744cd2df38c4d08df
devlin85 <devlin.skahill@gmail.com>
2015-04-15 01:25:29 -0400

    Creating TIT pool

---
+++ p2pool/networks/titcoin.py
@@ -0,0 +1,18 @@
+from p2pool.bitcoin import networks
+
+PARENT = networks.nets['titcoin']
+SHARE_PERIOD = 60 # seconds
+CHAIN_LENGTH = 24*60*60//10 # shares
+REAL_CHAIN_LENGTH = 24*60*60//10 # shares
+TARGET_LOOKBEHIND = 200 # shares
+SPREAD = 20 # blocks
+IDENTIFIER = 'fc70035c7a86969f'.decode('hex')
+PREFIX = '247696981efcd37b'.decode('hex')
+P2P_PORT = 8698
+MIN_TARGET = 0
+MAX_TARGET = 2**256//2**32 - 1
+PERSIST = False
+WORKER_PORT = 9666
+BOOTSTRAP_ADDRS = 'mining.p2pools.com rav3n.dtdns.net pool.hostv.pl p2pool.org seed.titcoins.info titcoin.hopto.org titcoin.isasecret.com titcoin.slyip.com titcoin.sytes.net 108.61.10.90 95.85.23.9 109.120.173.59 188.165.84.239'.split(' ')
+ANNOUNCE_CHANNEL = '#p2pool-alt'
+VERSION_CHECK = lambda v: True
