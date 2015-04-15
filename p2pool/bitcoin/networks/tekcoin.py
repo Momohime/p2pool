b0d067548215fda9a345fa208e9a6f448da92669
devlin85 <devlin.skahill@gmail.com>
2015-04-15 01:57:41 -0400

    Working TEK Pool

--- p2pool/bitcoin/networks/tekcoin.py
+++ p2pool/bitcoin/networks/tekcoin.py
@@ -9,7 +9,7 @@ from p2pool.util import pack
 
 P2P_PREFIX = 'e4e8e9e5'.decode('hex')
 P2P_PORT = 8514
-ADDRESS_VERSION = 0
+ADDRESS_VERSION = 26
 RPC_PORT = 18514
 RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
             'tekcoinaddress' in (yield bitcoind.rpc_help()) and
@@ -19,7 +19,7 @@ SUBSIDY_FUNC = lambda height: 1
 POW_FUNC = data.hash256
 BLOCK_PERIOD = 60 # s
 SYMBOL = 'TEK'
-CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'anarchistsprime') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/AnarchistsPrime/') if platform.system() == 'Darwin' else os.path.expanduser('~/.anarchistsprime'), 'anarchistsprime.conf')
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'tekcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/tekcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.tekcoin'), 'tekcoin.conf')
 BLOCK_EXPLORER_URL_PREFIX = 'http://tek.blockchain.lt/block/'
 ADDRESS_EXPLORER_URL_PREFIX = 'http://tek.blockchain.lt/address/'
 TX_EXPLORER_URL_PREFIX = 'http://tek.blockchain.lt/tx/'
