3e44365004d2723cf2dfa91d3ddeab1338369d04
devlin85 <devlin.skahill@gmail.com>
2015-04-15 01:58:15 -0400

    Working TEK Pool

--- p2pool/networks/tekcoin.py
+++ p2pool/networks/tekcoin.py
@@ -1,19 +1,13 @@
 from p2pool.bitcoin import networks
 
-# CHAIN_LENGTH = number of shares back client keeps
-# REAL_CHAIN_LENGTH = maximum number of shares back client uses to compute payout
-# REAL_CHAIN_LENGTH must always be <= CHAIN_LENGTH
-# REAL_CHAIN_LENGTH must be changed in sync with all other clients
-# changes can be done by changing one, then the other
-
 PARENT = networks.nets['tekcoin']
-SHARE_PERIOD = 180 # seconds
+SHARE_PERIOD = 60 # seconds
 CHAIN_LENGTH = 24*60*60//10 # shares
 REAL_CHAIN_LENGTH = 24*60*60//10 # shares
 TARGET_LOOKBEHIND = 200 # shares
 SPREAD = 20 # blocks
-IDENTIFIER = 'fc70035c7a81123f'.decode('hex')
-PREFIX = '247123181efcd37b'.decode('hex')
+IDENTIFIER = 'fc70035ca1e5223f'.decode('hex')
+PREFIX = '2475223f1efcd37b'.decode('hex')
 P2P_PORT = 8514
 MIN_TARGET = 0
 MAX_TARGET = 2**256//2**32 - 1
