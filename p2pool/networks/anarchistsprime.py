84da19e69d42052e8d36d7127074de9c8789e9af
devlin85 <devlin.skahill@gmail.com>
2015-04-16 14:29:24 -0400

    updated chain length and share period

--- p2pool/networks/anarchistsprime.py
+++ p2pool/networks/anarchistsprime.py
@@ -7,18 +7,18 @@ from p2pool.bitcoin import networks
 # changes can be done by changing one, then the other
 
 PARENT = networks.nets['anarchistsprime']
-SHARE_PERIOD = 180 # seconds
-CHAIN_LENGTH = 24*60*60//10 # shares
-REAL_CHAIN_LENGTH = 24*60*60//10 # shares
+SHARE_PERIOD = 5 # seconds
+CHAIN_LENGTH = 24*60*60//5 # shares
+REAL_CHAIN_LENGTH = 12*60*60//5 # shares
 TARGET_LOOKBEHIND = 200 # shares
-SPREAD = 20 # blocks
+SPREAD = 30 # blocks
 IDENTIFIER = 'fc70035c7a81666f'.decode('hex')
 PREFIX = '247666181efcd37b'.decode('hex')
 P2P_PORT = 11050
 MIN_TARGET = 0
 MAX_TARGET = 2**256//2**32 - 1
-PERSIST = True
+PERSIST = False
 WORKER_PORT = 9666
-BOOTSTRAP_ADDRS = 'mining.p2pools.com rav3n.dtdns.net pool.hostv.pl p2pool.org acp.explorer.ssdpool.com 52.16.23.106 52.17.116.144 178.62.102.181 173.16.173.143 104.155.18.57 128.199.111.217 109.88.89.62 58.7.251.128'.split(' ')
+BOOTSTRAP_ADDRS = 'rav3n.dtdns.net pool.hostv.pl p2pool.org acp.explorer.ssdpool.com 52.16.23.106 52.17.116.144 178.62.102.181 173.16.173.143 104.155.18.57 128.199.111.217 109.88.89.62 58.7.251.128'.split(' ')
 ANNOUNCE_CHANNEL = '#p2pool-alt'
 VERSION_CHECK = lambda v: True
