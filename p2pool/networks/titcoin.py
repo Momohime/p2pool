9921a7bae8379ea3f2b3733a75320b9124b4b668
devlin85 <devlin.skahill@gmail.com>
2015-04-24 01:25:28 -0400

    updated tit pool

--- p2pool/networks/titcoin.py
+++ p2pool/networks/titcoin.py
@@ -1,11 +1,11 @@
 from p2pool.bitcoin import networks
 
 PARENT = networks.nets['titcoin']
-SHARE_PERIOD = 60 # seconds
-CHAIN_LENGTH = 24*60*60//10 # shares
-REAL_CHAIN_LENGTH = 24*60*60//10 # shares
-TARGET_LOOKBEHIND = 200 # shares
-SPREAD = 20 # blocks
+SHARE_PERIOD = 30 # seconds
+CHAIN_LENGTH = 24*60*60//1 # shares
+REAL_CHAIN_LENGTH = 12*60*60//4 # shares
+TARGET_LOOKBEHIND = 69 # shares
+SPREAD = 69 # blocks
 IDENTIFIER = 'fc70035c7a86969f'.decode('hex')
 PREFIX = '247696981efcd37b'.decode('hex')
 P2P_PORT = 8698
